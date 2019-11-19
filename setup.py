32bb89f281d8858fea399ba8550f63ebe60e2d25
Arnaud Belcour <arnaud.belcour@irisa.fr>
2019-11-19 17:15:41 +0100

    Move to 1.1.5.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='Metage2Metabo',
-    version='1.1.4',
+    version='1.1.5',
     url='https://github.com/aureme/metage2metabo',
     license='GPLv3+',
     description=
