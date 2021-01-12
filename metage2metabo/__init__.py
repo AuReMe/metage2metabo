f16e7738d67e03f3843e279b2b494cfa5066afe5
Arnaud Belcour <arnaud.belcour@inria.fr>
2021-01-12 17:35:56 +0100

    Remove import in __init__.py.

--- metage2metabo/__init__.py
+++ metage2metabo/__init__.py
@@ -1,6 +1,3 @@
-from metage2metabo.m2m import run_workflow, recon, iscope, cscope, instance_community, addedvalue, mincom, metacom_analysis
-from metage2metabo.m2m_analysis import run_analysis_workflow, enumeration_analysis, powergraph_analysis, check_oog_jar_file, graph_analysis, stat_analysis
-
 __version__ = '1.4.1'
 """ version of the package
 """ 
