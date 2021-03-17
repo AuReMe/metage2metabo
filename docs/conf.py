84f3d46750089f4d1cbf9583e9862160c18a4c5a
Arnaud Belcour <arnaud.belcour@inria.fr>
2021-03-17 19:11:47 +0100

    Update Sphinx conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,11 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-]
+    'sphinx.ext.napoleon'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
